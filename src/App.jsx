b8e576bc3fa25949506dfb63db08030a73733d6b
Maulana Yusup <maulana.yusup8989@gmail.com>
2023-07-23 15:16:20 +0700

    Componen & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import "./App.css";
+import React from "react";
 
+const Button = (props) => {
+  const { variant, children } = props; // example default value: {varian = bg-red-500}, children = Login
+  return (
+    <button
+      class={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex justify-center bg-black min-h-screen items-center">
+      <div className="flex gap-x-2">
+        {/* //login and register are a special component that can capture tags within the component */}
+        <Button variant="bg-red-500">Register</Button>
+        <Button variant="bg-blue-500">Login</Button>
+      </div>
+    </div>
   );
 }
 
