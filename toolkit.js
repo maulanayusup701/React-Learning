35f46d4271d4abc111fda01bca3e988dd3e840d9
Maulana Yusup <maulana.yusup8989@gmail.com>
2023-07-27 19:54:05 +0700

    Redux Toolkit

---
+++ toolkit.js
@@ -0,0 +1,33 @@
+import toolkit from "@reduxjs/toolkit";
+const { configureStore, createReducer, createAction } = toolkit;
+
+const addToCart = createAction("ADD_TO_CART");
+
+const cartReducer = createReducer([], (builder) => {
+  builder.addCase(addToCart, (state, action) => {
+    state.push(action.payload);
+  });
+});
+
+const login = createAction("CREATE_SESSION");
+const loginReducer = createReducer({ status: false }, (builder) => {
+  builder.addCase(login, (state, action) => {
+    state.status = true;
+  });
+});
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    login: loginReducer
+  }
+});
+console.log("oncreate store", store.getState());
+
+store.subscribe(() => {
+  console.log("STORE CHANGED", store.getState());
+});
+
+store.dispatch(addToCart({ id: 1, qty: 20 }));
+store.dispatch(addToCart({ id: 1, qty: 20 }));
+store.dispatch(login());
